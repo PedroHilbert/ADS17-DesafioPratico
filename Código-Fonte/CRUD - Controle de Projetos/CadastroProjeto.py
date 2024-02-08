034cb5000fb63a8a1849a7d8d0639ad3210d5189
Pedro Hilbert Silva Guerra <pedrohilbertsg@gmail.com>
2024-02-08 16:12:09 -0300

    Adicionando Select

--- Código-Fonte/CRUD - Controle de Projetos/CadastroProjeto.py
+++ Código-Fonte/CRUD - Controle de Projetos/CadastroProjeto.py
@@ -9,19 +9,28 @@ conexao = mysql.connector.connect(
 )
 
 cursor = conexao.cursor()
-
+'''
 nomeprojeto = input('Qual o nome do projeto que deseja cadastrar? ')
 localizacao = input('Qual o endereço do seu projeto? ')
 escopo = input('Endereço do escopo: ')
 cliente = input('Nome do Cliente: ')
 dataInicioPrevista = input('Data de inicio da obra? ')
-
-# PARA RODAR O CÓDIGO,  
+'''
+# PARA RODAR O CÓDIGO, DEVE ESCOLHER O BLOCO DE CÓDIGO E RODA-LÓ
 
 #Aqui iremos adicionar um projeto no nosso banco de dados
+'''
 comando = f'INSERT INTO projetos (nome_projeto, localizacao, escopo, cliente, data_inicio) VALUES ("{nomeprojeto}", "{localizacao}", "{escopo}","{cliente}","{dataInicioPrevista}")'
 cursor.execute(comando)
 conexao.commit()
+'''
+#Aqui em baixo estaremos pesquisando os projetos cadastrado
+comando = f'SELECT * FROM projetos'
+cursor = conexao.cursor()
+cursor.execute(comando)
+
+resultado = cursor.fetchall() # ler o banco de dados
+print(resultado)
 
 
 
